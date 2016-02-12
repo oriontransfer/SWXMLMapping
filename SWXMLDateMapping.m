8169044420b879d9802c90dede04596bc6a00a1c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-02-13 01:44:54 +1300

    Handle nil and include iso8601 date value.

--- SWXMLDateMapping.m
+++ SWXMLDateMapping.m
@@ -11,29 +11,45 @@
 
 @implementation SWXMLDateMapping
 
+// ISO8601
+- (NSString *) standardDateString: (NSDate *)date {
+	NSDateFormatter *dateFormatter = [NSDateFormatter new];
+	NSLocale *enUSPOSIXLocale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+	[dateFormatter setLocale:enUSPOSIXLocale];
+	[dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZZZZZ"];
+
+	return [dateFormatter stringFromDate:date];
+}
+
 - serializedObjectMember: (id)object withMapping: (SWXMLMapping*)mapping {
 	NSDateFormatter *dateFormatter = [NSDateFormatter new];
 	NSTimeZone *timezone = nil;
 	NSLocale *locale = nil;
 	NSDate *date = [object valueForKeyPath:[self keyPath]];
-	
-	NSString *format;
-	format = [self.attributes valueForKey:@"format"];
-	
-	if (!format)
-		format = @"yyyy.MM.dd";
 
-	[dateFormatter setDateFormat:format];
-	
-	NSString *timezoneName = [self.attributes valueForKey:@"timezone"];
-	if (timezoneName)
-		dateFormatter.timeZone = [NSTimeZone timeZoneWithName:timezoneName];
-	
-	NSString *localeName = [self.attributes valueForKey:@"locale"];
-	if (localeName)
-		dateFormatter.locale = [NSLocale localeWithLocaleIdentifier:localeName];
+	if (date) {
+		NSString *format;
+		format = [self.attributes valueForKey:@"format"];
+		
+		if (!format)
+			format = @"yyyy-MM-dd";
 
-	return [SWXMLTags tagNamed:self.tag forCDATA:[dateFormatter stringFromDate:date]];
+		[dateFormatter setDateFormat:format];
+		
+		NSString *timezoneName = [self.attributes valueForKey:@"timezone"];
+		if (timezoneName)
+			dateFormatter.timeZone = [NSTimeZone timeZoneWithName:timezoneName];
+		
+		NSString *localeName = [self.attributes valueForKey:@"locale"];
+		if (localeName)
+			dateFormatter.locale = [NSLocale localeWithLocaleIdentifier:localeName];
+
+		return [SWXMLTags tagNamed:self.tag forCDATA:[dateFormatter stringFromDate:date] withAttributes:@{
+			@"value": [self standardDateString:date]
+		}];
+	} else {
+		return nil;
+	}
 }
 
 @end
